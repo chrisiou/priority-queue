4a96633247037febd8d3731fb65c197af96387f6
chrisiou <chrisiou.myk@gmail.com>
2021-07-27 22:36:02 +0200

    Implemented empty() method

---
+++ priority_queue.cpp
@@ -0,0 +1,6 @@
+#include "./priority_queue.h"
+
+template <typename T>
+bool Priority_queue<T>::empty(void) {
+    return (_size == 0);
+}
--- testing/tests.cpp
+++ testing/tests.cpp
@@ -1,5 +1,12 @@
 #include <gtest/gtest.h>
 #include "../priority_queue.h"
+#include "../priority_queue.cpp"
+
+TEST( test_priority_queue, empty)
+{
+   Priority_queue<int> q;
+   ASSERT_TRUE(q.empty());
+}
 
 int main(int argc, char **argv)
 {
