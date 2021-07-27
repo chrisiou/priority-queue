7ab1c3ad1d52d7dd146e9e786a9235aa3d38b34b
chrisiou <chrisiou.myk@gmail.com>
2021-07-27 22:41:16 +0200

    Implemented destructor

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -1,6 +1,11 @@
 #include "./priority_queue.h"
 
 template <typename T>
+Priority_queue<T>::~Priority_queue() {
+    delete[] data;
+}
+
+template <typename T>
 bool Priority_queue<T>::empty(void) {
     return (_size == 0);
 }
